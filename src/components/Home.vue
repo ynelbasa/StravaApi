57429f83c9b3ae63ad13d0c2dd594d0061285e1c
Ynel Basa <marinelbasa@gmail.com>
2020-09-09 17:03:30 +1200

    Display athlete detail on home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <v-container> {{ data }}</v-container>
+  <v-container>
+    <v-row no-gutters style="flex-wrap: nowrap;" justify="center">
+      <v-col class="flex-grow-0 flex-shrink-0">
+        <v-img class="rounded-circle" :src="user.profile" height="120" width="120"></v-img>
+        <h2 class="text-center">{{user.firstname}} {{user.lastname}}</h2>
+        <p class="text-center">{{user.country}}</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -7,18 +15,20 @@ export default {
   name: "Home",
   data() {
     return {
-      data: null,
+      user: null,
     };
   },
   mounted() {
-    var accessToken = "136d1239c5da4a83fd7385541c88e065a2b4311b";
+    var accessToken = "ceb3c686e67a4fd557915a29618705bd1b9e8647";
+
+    // Get Athlete
     this.$http
       .get("https://www.strava.com/api/v3/athlete", {
         headers: {
           Authorization: "Bearer " + accessToken,
         },
       })
-      .then((response) => (this.data = response));
+      .then((response) => (this.user = response.data));
   },
 };
 </script>
