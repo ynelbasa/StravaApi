e1bc1a7a037077c95ee7ec30420eb3915f535dc9
Ynel Basa <marinelbasa@gmail.com>
2020-09-12 18:50:07 +1200

    Add more content on profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,13 +1,33 @@
 <template>
-<v-container>
-  <v-row no-gutters style="flex-wrap: nowrap;" justify="center">
-    <v-col class="flex-grow-0 flex-shrink-0" v-if="user">
-      <v-img class="rounded-circle" :src="user.profile" height="120" width="120"></v-img>
-      <h2 class="text-center">{{ user.firstname }} {{ user.lastname }}</h2>
-      <body class="text-center">
+<v-container v-if="user">
+  <v-row no-gutters style="flex-wrap: nowrap;" class="mt-5" justify="center">
+    <v-col cols="4" align="center">
+      <v-img class="rounded-circle" :src="user.profile" height="180" width="180"></v-img>
+      <h2>{{ user.firstname }} {{ user.lastname }}</h2>
+      <body>
         <i class="fas fa-map-marker-alt"></i>
         {{ user.country }}
       </body>
+    </v-col>
+    <v-col align="left" class="mt-15">
+      <h4>
+        Membership status:
+        <span
+          class="font-weight-regular"
+        >{{ user.summit ? 'Free' : 'Summit Member'}}</span>
+      </h4>
+      <h4>
+        Profile created at:
+        <span
+          class="font-weight-regular"
+        >{{ moment(user.created_at).format("YYYY-MM-DD HH:mm") }}</span>
+      </h4>
+      <h4>
+        Last updated at:
+        <span
+          class="font-weight-regular"
+        >{{ moment(user.updated_at).format("YYYY-MM-DD HH:mm") }}</span>
+      </h4>
     </v-col>
   </v-row>
 </v-container>
