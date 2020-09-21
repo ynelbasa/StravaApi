65346f293de4f9d171c40990c6e5b0a946c06107
Ynel Basa <marinelbasa@gmail.com>
2020-09-21 18:12:35 +1200

    Get refreshed access token on app start

--- src/main.js
+++ src/main.js
@@ -8,11 +8,15 @@ import router from './router'
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 Vue.prototype.moment = moment
-Vue.prototype.$accessCode = 'aa73dc83b4afd6e36f6d12a15a23137facf5a615'
 Vue.prototype.$googleApiKey = 'AIzaSyAYYWSEDiyx7lomBF9IX-lZdASo4C8G9S4'
 
-new Vue({
-  vuetify,
-  router,
-  render: h => h(App)
-}).$mount('#app')
+axios.post("https://www.strava.com/api/v3/oauth/token?client_id=51311&client_secret=182e267e5d263a569a8b56c1755563af7f87e8a1&grant_type=refresh_token&refresh_token=223f998757623581acc1c7183c764bf8149399d4")
+  .then((response) => {
+    Vue.prototype.$accessCode = response.data.access_token;
+
+    new Vue({
+      vuetify,
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+  });
